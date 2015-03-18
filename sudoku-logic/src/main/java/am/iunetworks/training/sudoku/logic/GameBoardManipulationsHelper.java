94f476515d7b6e3a73fddb543d660b70e23dbd29
Artiom Sarkisyan <artiom.sar@gmail.com>
2015-03-18 16:12:13 +0400

    add new metods-rotateRight+its test-Metods-replaceRow,add new test-Metods- rotate+rotTest-testReplaceRow+swichTest

--- sudoku-logic/src/main/java/am/iunetworks/training/sudoku/logic/GameBoardManipulationsHelper.java
+++ sudoku-logic/src/main/java/am/iunetworks/training/sudoku/logic/GameBoardManipulationsHelper.java
@@ -1,5 +1,5 @@
 package am.iunetworks.training.sudoku.logic;
-
+import java.util.Arrays;
 /**
  * This class contains all board manipulations.
  *  - Rotate
@@ -12,12 +12,15 @@ public class GameBoardManipulationsHelper {
 
     private char[] board;
 
+    private int range = (int)(Math.random()*8);
+    private int range2 = (int)(Math.random()*8);
+
     public GameBoardManipulationsHelper(String board) {
         checkIsBoardValid(board);
         this.board = board.toCharArray();
     }
 
-    private void checkIsBoardValid(String board) {
+      private void checkIsBoardValid(String board) {
         if (board == null) {
             throw new NullPointerException("Game board can't be null");
         }
@@ -29,5 +32,86 @@ public class GameBoardManipulationsHelper {
             throw new IllegalArgumentException("Game board can only have 0-9 chars");
         }
     }
+
+
+
+
+    public char[] getBoard() {
+        return board;
+    }
+    public int getRange() {
+        return range;
+    }
+    public int getRange2() {
+        return range2;
+    }
+
+
+    //rotate metod
+    protected char[] rotateRight() {
+
+        int length=9;
+
+        char [][] temp =new char[length][length];
+        int index= 0;
+
+        for (int i = 0; i < length;i++ ){
+            for(int j = 0; j < length; j++) {
+                temp[i][j] = board[index];
+                index++;
+
+            }
+        }
+
+        int index2=0;
+        for (int i = 0; i < length;i++ ){
+            for(int j = 0; j < length; j++) {
+                board[index2] = temp[length-j-1][i];
+                index2++;
+            }
+
+        }
+
+        return board;}
+
+
+
+    //Row swich or replace manipulation
+    protected char[] replaceRow(){
+
+
+        int length=9;
+
+        char [][] matrix =new char[length][length];
+        char [][] temp =new char[length][length];
+        int index= 0;
+
+        for (int i = 0; i < length;i++ ){
+            for(int j = 0; j < length; j++) {
+                temp[i][j] = board[index];
+                index++;
+
+            }
+        }
+
+        int index2=0;
+         for (int i = 0; i < length;i++ ){
+            for(int j = 0; j < length; j++) {
+                if (j == range  ||  j==range2){
+                    matrix [i][range]=temp[i][range2];
+                    matrix [i][range2]=temp[i][range];
+
+                }
+                else{ matrix [i][j]=temp[i][j]; }
+
+                board[index2] = matrix[i][j];
+                index2++;
+
+            }
+
+        }
+
+        return board;}
+
 
 }
