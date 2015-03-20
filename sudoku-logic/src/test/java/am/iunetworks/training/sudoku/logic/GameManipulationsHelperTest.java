453bc0439f0a53ec3339e81ca487adceedac86ef
Anahit92 <anahitmirzoyan92@gmail.com>
2015-03-20 07:03:29 +0400

    rotating_board

--- sudoku-logic/src/test/java/am/iunetworks/training/sudoku/logic/GameManipulationsHelperTest.java
+++ sudoku-logic/src/test/java/am/iunetworks/training/sudoku/logic/GameManipulationsHelperTest.java
@@ -45,4 +45,76 @@ public class GameManipulationsHelperTest {
         Arrays.fill(board,'0');
         return new String(board);
     }
+    @Test
+    public void rotateTest(){
+
+        int[][] board = new int[9][9];
+        int[][] newBoard = new int[9][9];
+        board[0][0]=1;
+        board[0][1]=2;
+        board[0][2]=3;
+        board[0][3]=4;
+        board[0][4]=5;
+        board[0][5]=6;
+        board[0][6]=7;
+        board[0][7]=8;
+        board[0][8]=9;
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+
+                newBoard[i][j] = board[i][j];
+            }
+        }
+        int rowLength = board.length;
+        int columnLength = board[0].length;
+        int i;
+        int j;
+        for ( i = 0; i < rowLength; i++){
+            System.out.println();
+            for ( j=0; j<columnLength;j++){
+                System.out.print(board[i][j]);
+            }
+        }
+        System.out.println();
+
+        GameBoardManipulationsHelper.rotate(board, board.length);
+        for ( i = 0; i < rowLength; i++){
+            System.out.println();
+            for ( j=0; j<columnLength;j++){
+
+                System.out.print(board[i][j]);
+            }
+        }
+        System.out.println();
+
+
+        GameBoardManipulationsHelper.rotate(board, board.length);
+        for ( i = 0; i < rowLength; i++){
+            System.out.println();
+            for ( j=0; j<columnLength;j++){
+                System.out.print(board[i][j]);
+            }
+        }
+        System.out.println();
+
+
+        GameBoardManipulationsHelper.rotate(board, board.length);
+        for ( i = 0; i < rowLength; i++){
+            System.out.println();
+            for ( j=0; j<columnLength;j++){
+                System.out.print(board[i][j]);
+            }
+        }
+        System.out.println();
+
+        GameBoardManipulationsHelper.rotate(board, board.length);
+        assertArrayEquals("Something was wrong with rotation", newBoard, board );
+        for ( i = 0; i < rowLength; i++){
+            System.out.println();
+            for ( j=0; j<columnLength;j++){
+                System.out.print(board[i][j]);
+            }
+        }
+    }
 }
