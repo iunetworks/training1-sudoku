7fe8463a00d42f599add7ffa36f5b4a531c520e3
Artiom Sarkisyan <artiom.sar@gmail.com>
2015-04-08 12:13:42 +0400

    new switch metod & switch test

--- sudoku-logic/src/main/java/am/iunetworks/training/sudoku/logic/GameBoardManipulationsHelper.java
+++ sudoku-logic/src/main/java/am/iunetworks/training/sudoku/logic/GameBoardManipulationsHelper.java
@@ -1,5 +1,4 @@
 package am.iunetworks.training.sudoku.logic;
-
 /**
  * This class contains all board manipulations.
  *  - Rotate
@@ -12,12 +11,15 @@ public class GameBoardManipulationsHelper {
 
     private char[] board;
 
-    public GameBoardManipulationsHelper(String board) {
+
+
+
+  public GameBoardManipulationsHelper(String board) {
         checkIsBoardValid(board);
         this.board = board.toCharArray();
     }
 
-    private void checkIsBoardValid(String board) {
+      private void checkIsBoardValid(String board) {
         if (board == null) {
             throw new NullPointerException("Game board can't be null");
         }
@@ -29,5 +31,48 @@ public class GameBoardManipulationsHelper {
             throw new IllegalArgumentException("Game board can only have 0-9 chars");
         }
     }
+
+
+
+
+    //Row switch or replace manipulation
+    public  char[] switchRow(int a,int b){
+        int range = a;
+        int range2 = b;
+
+        int length=9;
+
+        char [][] matrix =new char[length][length];
+        char [][] temp =new char[length][length];
+        int index= 0;
+
+        for (int i = 0; i < length;i++ ){
+            for(int j = 0; j < length; j++) {
+                temp[i][j] = board[index];
+                index++;
+
+            }
+        }
+
+        int index2=0;
+        for (int i = 0; i < length;i++ ){
+            for(int j = 0; j < length; j++) {
+                if (j == range  ||  j==range2){
+                    matrix [i][range]=temp[i][range2];
+                    matrix [i][range2]=temp[i][range];
+
+                }
+                else{ matrix [i][j]=temp[i][j]; }
+
+                board[index2] = matrix[i][j];
+                index2++;
+
+            }
+
+        }
+
+        return board;}
+
+
 
 }
