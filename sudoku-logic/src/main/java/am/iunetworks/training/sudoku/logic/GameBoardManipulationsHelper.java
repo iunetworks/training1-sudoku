d68b42e9edfba524a94a3773c9a311e3458c0c03
David <david.karchikyan@gmail.com>
2017-04-15 23:58:16 +0400

    Add rotate and switch method

--- sudoku-logic/src/main/java/am/iunetworks/training/sudoku/logic/GameBoardManipulationsHelper.java
+++ sudoku-logic/src/main/java/am/iunetworks/training/sudoku/logic/GameBoardManipulationsHelper.java
@@ -29,5 +29,48 @@ public class GameBoardManipulationsHelper {
             throw new IllegalArgumentException("Game board can only have 0-9 chars");
         }
     }
+    
+    public GameBoardManipulationsHelper rotateBoardOn90Angle() {
+		char[] boardRotate = new char[board.length];
+		int digitPosition = board.length - 1;
+		for (int i = 1; i < 10; i++) {
+			for (int j = board.length - (10 - i); j >= 0; j -= 9) {
+				boardRotate[j] = board[digitPosition];
+				digitPosition--;
+			}
+		}
+		board = boardRotate;
+		return this;
+	}
+    
+    public GameBoardManipulationsHelper switchRow(int selectRow, int changeRow) {
+		if (selectRow < 0 || selectRow > 8 || changeRow < 0 || changeRow > 8 || selectRow == changeRow) {
+			return this;
+		}
+		char tmp;
+		for (int i = 0; i < 9; i++) {
+			tmp = board[i + (changeRow * 9)];
+			board[i + (changeRow * 9)] = board[i+(selectRow*9)];
+			board[i+(selectRow*9)] = tmp;
+		}
+		return this;
+	}
+    
+    public GameBoardManipulationsHelper switchColumn(int selectColumn, int changeColumn) {
+		if (selectColumn < 0 || selectColumn > 8 || changeColumn < 0 || changeColumn > 8 || selectColumn == changeColumn) {
+			return this;
+		}
+		char tmp;
+		for(int i = 0; i < 9; i++) {
+			tmp = board[selectColumn + (9 * i)];
+			board[selectColumn + (9 * i)] = board[changeColumn + (9 * i)];
+			board[changeColumn + (9 * i)] = tmp;
+		}
+		return this;
+	}
+    
+    public String getBoard() {
+		return new String(board);
+	}
 
 }
