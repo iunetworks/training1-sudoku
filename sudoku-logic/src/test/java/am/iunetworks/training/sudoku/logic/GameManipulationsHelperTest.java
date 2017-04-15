e8848b5340b89ce4fd1119778af33fb9f7b9e8e8
David <david.karchikyan@gmail.com>
2017-04-15 23:59:20 +0400

    Add switch and rotate tests

--- sudoku-logic/src/test/java/am/iunetworks/training/sudoku/logic/GameManipulationsHelperTest.java
+++ sudoku-logic/src/test/java/am/iunetworks/training/sudoku/logic/GameManipulationsHelperTest.java
@@ -39,6 +39,38 @@ public class GameManipulationsHelperTest {
         // and simple normal case
         new GameBoardManipulationsHelper(getEmptyBoard());
     }
+    
+	@Test
+	public void testBoardRotate() {
+		// valid Board
+		String testBoard = "375964182964182375182375964759641823641823759823759641596418237418237596237596418";
+		GameBoardManipulationsHelper helper = new GameBoardManipulationsHelper(testBoard);
+		// rotate 90+90+90+90
+		helper.rotateBoardOn90Angle();
+		helper.rotateBoardOn90Angle();
+		helper.rotateBoardOn90Angle();
+		helper.rotateBoardOn90Angle();
+
+		assertEquals(testBoard, helper.getBoard());
+	}
+
+	@Test
+	public void testSwitchRowsAndColumns() {
+		//board
+		String testBoard = "375964182964182375182375964759641823641823759823759641596418237418237596237596418";
+		GameBoardManipulationsHelper helper = new GameBoardManipulationsHelper(testBoard);
+		//switch Rows and Columns
+		helper.switchRow(1, 3);
+		helper.switchColumn(2, 5);
+		helper.switchRow(3, 8);
+		helper.switchColumn(5, 0);
+		helper.switchRow(8, 1);
+		helper.switchColumn(0, 5);
+		helper.switchRow(8, 3);
+		helper.switchColumn(5, 2);
+		
+		assertEquals(testBoard, helper.getBoard());
+	}
 
     private String getEmptyBoard() {
         char[] board = new char[81];
